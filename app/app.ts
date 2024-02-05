5aded89bdde96986d3b09ff4a79d0622a566c507
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-02-05 16:49:37 +0530

    App.ts null object error resolved

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function electronMoves() {
+    var messageElement = document.getElementById('messages');
+    messageElement!.innerText = 'Voltage or a potential difference causes electrons to flow in a wire.';
+
+}
+document.getElementById('startFlow')!.addEventListener('click', electronMoves);
