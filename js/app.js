702592ec630970f37647ac08a1c630eb8741fadd
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-02-05 17:24:48 +0530

    new output directory & mapping locations

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function electronMoves() {
+    var name = "My Electronics App";
+    var messageElement = document.getElementById('messages');
+    messageElement.innerText = 'Voltage or a potential difference causes electrons to flow in a wire.';
+}
+document.getElementById('startFlow').addEventListener('click', electronMoves);
+//# sourceMappingURL=app.js.map
