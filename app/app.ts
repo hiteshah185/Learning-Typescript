9457fcf242bf60fac5b2ba19fe78cad759453b75
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-02-05 17:23:45 +0530

    added comments on worksheet

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 function electronMoves() {
+    var name = "My Electronics App"; //Naming
     var messageElement = document.getElementById('messages');
     messageElement!.innerText = 'Voltage or a potential difference causes electrons to flow in a wire.';
-
+    //A Sample message
 }
 document.getElementById('startFlow')!.addEventListener('click', electronMoves);
