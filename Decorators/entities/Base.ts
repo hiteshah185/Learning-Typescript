981962bff1249d5d489cce9dbac26990080a8d2b
hharik01 <Hitesh.Harikumar@teranet.ca>
2024-02-20 12:13:13 +0530

    Added fromPersistenceObject method - base.ts

--- Decorators/entities/Base.ts
+++ Decorators/entities/Base.ts
@@ -8,8 +8,17 @@ export interface IEntity {
 export class EntityFactory {
     static fromPersistenceObject<T extends IEntity>(Obj: Object, type: EntityTypeInstance<T>): T {
         let output = new type();
-        const persistedProperties:string[] = Ref
-        return {} as T;
+        const persistedProperties: string[] = Reflect.getMetadata("entity:properties", output) || [];
+        const idProperty = Reflect.getMetadata("entity:id", output);
+        const props = Object.keys(Obj);
+        for (const prop of props) {
+            if (persistedProperties.includes(prop) || prop == idProperty) {
+                output[prop] = Obj[prop];
+            } else {
+                throw new Error("Property not defined in class.");
+            }
+        }
+        return output;
     }
 }
 export default class BaseEntity implements IEntity {
