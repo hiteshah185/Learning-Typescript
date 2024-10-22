91ba50ce8f0ca9f7082fb2223ebe143dcad4c331
Hitesh A H <118159423+hiteshah185@users.noreply.github.com>
2024-10-22 12:31:42 +0530

    RxJs: of operator

--- standalone-scripts/RxJsOperators.ts
+++ standalone-scripts/RxJsOperators.ts
@@ -1,8 +1,20 @@
 import { from, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 
 
 const source1 = from([44, 55, 66, 77, 88, 99]);
 source1.pipe(
     map(val => val * 10)
 ).subscribe(val => console.log(`Mapped value: ${val}`));
+
+const source2 = of('a', 'b', 'c');
+source2.pipe(
+    filter(val => val % 2 === 0),
+    tap(val => print(`Tapped:${val}`))
+).subscribe(
+    val => print(`After subscribing:${val}`)
+);
+
+function print(message: string) {
+    console.log(message);
+}
