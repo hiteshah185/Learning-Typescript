d29ea826a0b7bc4676ac92b9a43dd27718fb0b9e
Hitesh A H <118159423+hiteshah185@users.noreply.github.com>
2024-10-22 13:13:23 +0530

    Updated Rxjs Example, installed @types

--- standalone-scripts/RxJsOperators.ts
+++ standalone-scripts/RxJsOperators.ts
@@ -1,19 +1,27 @@
-import { from, of } from 'rxjs';
-import { map, filter, tap } from 'rxjs/operators';
+import { from, of } from "rxjs";
+import { map, filter, tap, reduce, scan } from "rxjs/operators";
 
+//Creation Operators: of, from
+let source1 = from([44, 55, 66, 77, 88, 99]);
+source1
+    .pipe(map((val) => val * 10))
+    .subscribe((val) => console.log(`Mapped value: ${val}`));
 
-const source1 = from([44, 55, 66, 77, 88, 99]);
-source1.pipe(
-    map(val => val * 10)
-).subscribe(val => console.log(`Mapped value: ${val}`));
+let source2 = of("a", "b", "c");
+source2
+    .pipe(
+        filter((val) => val.toLowerCase() === "hi"),
+        tap((val) => print(`Tapped:${val}`))
+    )
+    .subscribe((val) => print(`After subscribing:${val}`));
+//Trasformation Operators: map, filter, pluck,tap
 
-const source2 = of('a', 'b', 'c');
-source2.pipe(
-    filter(val => val % 2 === 0),
-    tap(val => print(`Tapped:${val}`))
-).subscribe(
-    val => print(`After subscribing:${val}`)
-);
+source1
+    .pipe(
+        reduce((acc, val) => acc + val, 0),
+        scan((acc, val) => acc + val, 0)
+    )
+    .subscribe((val) => print(`After subscribing:${val}`));
 
 function print(message: string) {
     console.log(message);
