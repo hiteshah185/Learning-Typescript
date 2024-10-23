8bbae9b413c12ff4277c17ea6fb8433c2037cfe6
Hitesh A H <118159423+hiteshah185@users.noreply.github.com>
2024-10-23 11:46:43 +0530

    RxJS - Added Interval Source

--- standalone-scripts/RxJsOperators.ts
+++ standalone-scripts/RxJsOperators.ts
@@ -1,28 +1,43 @@
-import { from, of } from "rxjs";
-import { map, filter, tap, reduce, scan } from "rxjs/operators";
+import { from, of, interval } from "rxjs";
+import {
+  map,
+  filter,
+  tap,
+  reduce,
+  scan,
+  take,
+  takeUntil,
+  first,
+  debounceTime,
+} from "rxjs/operators";
 
 //Creation Operators: of, from
 let source1 = from([44, 55, 66, 77, 88, 99]);
 source1
-    .pipe(map((val) => val * 10))
-    .subscribe((val) => console.log(`Mapped value: ${val}`));
+  .pipe(map((val) => val * 10))
+  .subscribe((val) => console.log(`Mapped value: ${val}`));
 
 let source2 = of("a", "b", "c");
 source2
-    .pipe(
-        filter((val) => val.toLowerCase() === "hi"),
-        tap((val) => print(`Tapped:${val}`))
-    )
-    .subscribe((val) => print(`After subscribing:${val}`));
+  .pipe(
+    filter((val) => val.toLowerCase() === "a"),
+    tap((val) => print(`Tapped:${val}`))
+  )
+  .subscribe((val) => print(`After subscribing:${val}`));
 //Trasformation Operators: map, filter, pluck,tap
 
 source1
-    .pipe(
-        reduce((acc, val) => acc + val, 0),
-        scan((acc, val) => acc + val, 0)
-    )
-    .subscribe((val) => print(`After subscribing:${val}`));
+  .pipe(
+    reduce((acc, val) => acc + val, 0),
+    scan((acc, val) => acc + val, 0)
+  )
+  .subscribe((val) => print(`After subscribing:${val}`));
+
+let source3 = interval(1000);
+source3
+  .pipe(take(3), takeUntil(source2.pipe(first())), debounceTime(500))
+  .subscribe((val) => print(`Debounced:${val}`));
 
 function print(message: string) {
-    console.log(message);
+  console.log(message);
 }
